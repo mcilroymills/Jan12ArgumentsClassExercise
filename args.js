7e1f7c0c7049298890c19156eb50085348581b42
millsmcilroy <millsmcilroy@gmail.com>
2016-01-12 17:55:55 -0700

    Added comments and line spacing to prints

--- args.js
+++ args.js
@@ -1,3 +1,7 @@
+/****************************************************************************
+This function takes any amount of arguments and prints messages according to 
+the assignment instructions starting on line 37.
+*****************************************************************************/
 function myFn(first, second, third) {
 	if (arguments.length === 0)
 		console.log("No arguments!");
@@ -15,10 +19,65 @@ function myFn(first, second, third) {
 }
 
 myFn();
+console.log();
 myFn("first");
+console.log();
 myFn("first", "second");
+console.log();
 myFn(undefined, "second");
+console.log();
 myFn("first", "second", "third");
+console.log();
 myFn("first", undefined, "third");
+console.log();
 myFn("first", "second", "third", "fourth");
+console.log();
 myFn(undefined, "second", undefined, "fourth");
+
+// The following code...
+
+//myFn();
+//myFn("first");
+//myFn("first", "second");
+//myFn(undefined, "second");
+//myFn("first", "second", "third");
+//myFn("first", undefined, "third");
+//myFn("first", "second", "third", "fourth");
+//myFn(undefined, "second", undefined, "fourth");
+
+// Should return the following...
+
+// No arguments!
+
+// Beginning to read arguments...
+// Reading argument #1: first
+
+// Beginning to read arguments...
+// Reading argument #1: first
+// Reading argument #2: second
+
+// Beginning to read arguments...
+// Reading argument #2: second
+
+// Beginning to read arguments...
+// Reading argument #1: first
+// Reading argument #2: second
+// Reading argument #3: third
+
+// Beginning to read arguments...
+// Reading argument #1: first
+// Reading argument #3: third
+
+// Beginning to read arguments...
+// Reading argument #1: first
+// Reading argument #2: second
+// Reading argument #3: third
+
+// AARG! Too many arguments:
+//  { '0': 'first', '1': 'second', '2': 'third', '3': 'fourth' }
+
+// Beginning to read arguments...
+// Reading argument #2: second
+
+// AARG! Too many arguments:
+//  { '0': undefined, '1': 'second', '2': undefined, '3': 'fourth' }
